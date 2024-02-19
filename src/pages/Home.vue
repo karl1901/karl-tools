73a873356517d8e6abe4370cac78bb85289f087f
karl <2371265527@qq.com>
2024-02-19 09:10:56 +0800

    最新

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,54 +1,26 @@
 <template>
     <div class="body">
         <div>{{ title }}</div>
-        <div class="values">
-            <CountTo :startValue="0" :endValue="values" :duration="800" :prefix="'单位：'" :suffix="'＄'"></CountTo>
-        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import CountTo from '@/components/CountTo.vue';
 
 export default defineComponent({
     name: 'Home',
-    components: {
-        CountTo
-    },
     data() {
         return {
-            title: '首页',
-            iv: null as any,
-            values: 237923
+            title: '首页'
         };
     },
-    mounted() {
-        this.iv = setInterval(() => {
-            this.values = this.generateRandomInteger(131, 231132);
-        }, 3000);
-    },
-    methods: {
-        generateRandomInteger(min: any, max: any) {
-            // 使用 Math.floor() 向下取整，确保生成的是整数
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        }
-    },
-    beforeUnmount() {
-        clearInterval(this.iv as any);
-        this.iv = null;
-    }
+    mounted() {},
+    methods: {},
+    beforeUnmount() {}
 });
 </script>
 
 <style lang="scss" scoped>
-.values {
-    font-size: 2rem;
-    font-weight: bold;
-    font-family: '黑体';
-    color: #255cf3;
-}
-
 .body {
     @include bodyStylePage;
 }
